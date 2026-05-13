09c39dd8567dae48813a586da42bbffc32de65e5
collusion <galitskynikolai@yandex.ru>
2026-05-13 09:52:47 +0300

    Update Login component layout for improved responsiveness
    - Adjusted the main container to use flex-1 for better space utilization.
    - Changed child div's class to items-stretch for consistent alignment of elements.

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -3,8 +3,8 @@ import { GalleryVerticalEnd } from "lucide-react";
 
 export const Login = () => {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+    <div className="bg-muted flex min-h-svh w-full flex-1 flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col items-stretch gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <GalleryVerticalEnd className="size-4" />
